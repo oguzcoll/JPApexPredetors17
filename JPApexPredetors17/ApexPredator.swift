ce45dddfa1ecb51e42c8e540a7b4dbd6a7f81973
oguzcoll <oguzcolak2000@gmail.com>
2024-04-25 12:17:34 +0300

    image compund property, PredatorType enum and lList view added

--- JPApexPredetors17/ApexPredator.swift
+++ JPApexPredetors17/ApexPredator.swift
@@ -6,20 +6,43 @@
 //
 
 import Foundation
-struct ApexPredator : Decodable{
+import SwiftUI
+
+struct ApexPredator : Decodable , Identifiable{
     let id: Int
     let name: String
-    let type: String
+    let type: PredatorType
     let latitude: Double
     let longitude: Double
     let movies: [String]
     let movieScenes: [MovieScene]
     let link:String
     
+    var image:String{
+        name.lowercased().replacingOccurrences(of: " ", with: "")
+    }
     
     struct MovieScene : Decodable {
         let id:Int
         let movie:String
         let sceneDescription:String
     }
+    
+    enum PredatorType:String, Decodable{
+        case land
+        case air
+        case sea
+        
+        var background: Color{
+            switch self {
+            case .land:
+                .brown
+            case .air:
+                .teal
+            case .sea:
+                .blue
+            }
+        }
+    }
+
 }
