accec911580ef28acd5640a24e22f4434f5f648e
oguzcoll <oguzcolak2000@gmail.com>
2024-04-25 11:42:39 +0300

    build apexpredator model and predator class for decoding json data

---
+++ JPApexPredetors17/Predators.swift
@@ -0,0 +1,31 @@
+//
+//  Predators.swift
+//  JPApexPredetors17
+//
+//  Created by Oguz Colak on 25.04.2024.
+//
+
+import Foundation
+
+class Predators{
+    var apexPredators: [ApexPredator] = []
+    
+    init(){
+        decodeApexPredatorData()
+    }
+    
+    func decodeApexPredatorData(){
+        if let url = Bundle.main.url(forResource: "jpapexpredators", withExtension: "json"){
+            do{
+                let data = try Data(contentsOf: url)
+                let decoder = JSONDecoder()
+                decoder.keyDecodingStrategy = .convertFromSnakeCase
+                apexPredators = try decoder.decode([ApexPredator].self, from: data)
+            }catch{
+                print("Error decoding JSON data: \(error)")
+            }
+        }
+    }
+        
+    
+}
