742e9a3bb0374a6262ba6483e71a7074d8e4dfa3
oguzcoll <oguzcolak2000@gmail.com>
2024-04-25 10:53:52 +0300

    Initial Commit

---
+++ JPApexPredetors17/JPApexPredetors17App.swift
@@ -0,0 +1,17 @@
+//
+//  JPApexPredetors17App.swift
+//  JPApexPredetors17
+//
+//  Created by Oguz Colak on 25.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct JPApexPredetors17App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
